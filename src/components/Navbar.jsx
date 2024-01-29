6e580c820410626d1f7881f36471c879afd1cafe
nabeel matech <nabeel@matechco.com>
2024-01-29 15:19:58 +0500

    Navbar Styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import { React } from 'react';
 import { Link } from 'react-router-dom';
 import "../assets/css/navbar.css"
-import { Navbar, Nav, Collapse, NavDropdown, Container } from 'react-bootstrap';
+import { Navbar, Nav, Collapse, NavDropdown, Container, Form, Button } from 'react-bootstrap';
 
 function Header() {
     return (
         <>
-            <Navbar expand="lg" className='py-lg-5 py-4'>
+            <Navbar expand="lg" className='py-4'>
                 <Container fluid="lg">
                     <Navbar.Brand>
                         <Link to="/">
@@ -18,15 +18,25 @@ function Header() {
                             <i class="fa-solid fa-bars open"></i>
                             <i class="fa-solid fa-xmark close"></i>
                         </Navbar.Toggle>
+                        <Form className="d-flex headerForm border-1">
+                            <Form.Control
+                                type="search"
+                                placeholder="Search 10,000 Products"
+                                className=""
+                                aria-label="Search"
+                            />
+                            <Button className='bg-light text-dark border-0'><i class="fa-solid fa-magnifying-glass"></i></Button>
+                        </Form>
                         <Navbar.Collapse id="navbar-nav">
                             <Nav className="w-100 justify-content-center">
-                                <Link to="/about" className='nav-link'>About</Link>
-                                <Link to="/services" className='nav-link'>Services</Link>
-                                <Link to="/contacts" className='nav-link'>Contacts</Link>
-                                <Link to="/faq" className='nav-link'>FAQ</Link>
+                                <Link to="/about" className='nav-link'>Home</Link>
+                                <Link to="/about-us" className='nav-link'>About Us</Link>
+                                <Link to="/accessories" className='nav-link'>Accessories</Link>
+                                <Link to="/apparels" className='nav-link'>Apparels</Link>
+                                <Link to="/jackets" className='nav-link'>Jackets</Link>
+                                <Link to="/contact" className='nav-link'>Contact</Link>
                             </Nav>
                         </Navbar.Collapse>
-                        <Link href="/sign-up" className='navbar-link'>Sign Up</Link>
                     </div>
                 </Container>
             </Navbar>
